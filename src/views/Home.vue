bdbd6d3f1a642572bd3347363eb3728066753261
inneobr <tecnologiaesolucoespr@gmail.com>
2023-10-03 16:07:59 -0300

    feat: home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,8 +4,13 @@
       <h2 data-aos="zoom-in">Bem vindo!</h2>
       <p data-aos="flip-up" data-aos-duration="2000">
         Você está em um ambiente seguro, então pode ficar tranquilo.<br />
-        Este portfolio foi desenvolvido em Vue.js e Java Spring Boot.
-      </p>     
+        Este portfolio foi desenvolvido em Vue.js e Java Spring Boot.<br />
+        Atualmente roda em um servidor Linux com container Docker.
+      </p>  
+      <p data-aos="flip-up" data-aos-duration="2000">
+        Ele possui diversas funcionalidades, mas você está vendo<br /> 
+        apenas 0,01% de todos os recursos disponíveis. 
+      </p>  
     </div>
   </section>
 </template>
