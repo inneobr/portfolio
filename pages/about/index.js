ade95c8a076be0cad3e755a3e566011fa2dac5cd
eduardo.cruz <tecnologiaesolucoespr@gmail.com>
2023-10-11 15:53:43 -0300

    feat: docker

--- pages/about/index.js
+++ pages/about/index.js
@@ -106,15 +106,27 @@ const About = () => {
         <motion.div         
           variants={fadeIn('right', 0.2)} 
           initial='hidden' 
-          animate='hidden' 
+          animate='show' 
           exit='hidden' 
           className="hidden xl:flex absolute bottom-0 -left-[370px]"
         >
           <Avatar />
         </motion.div>
         <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
-          <div className="flex-1 flex flex-col justify-center">text</div>
-          <div className="flex flex-col w-full xl:max-[48%] h-[480px]">
+          {/* text */}
+          <div className="flex-1 flex flex-col justify-center">
+            <h2 className="h2">Histórias <span className="text-accent">cativantes</span> geram designs magníficos</h2>
+            <p className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0 text-justify">              
+              Há 6 anos, comecei a trabalhar como freelancer como desenvolvedor. 
+              Desde então, fiz trabalho remoto para agências, 
+              prestei consultoria para startups e colaborei em produtos digitais para uso empresarial e de consumo.
+            </p>
+            {/* counters */}
+            <div>counters</div>
+          </div>
+
+          {/* info */}
+          <div className="flex flex-col w-full xl:max-w-[48%] h-[480px]">
             <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
               {aboutData.map((item, itemIndex) => { 
                 return (
@@ -134,16 +146,16 @@ const About = () => {
               items-center xl:items-start">
               {aboutData[index].info.map((item, itemIndex) => {
                 return (
-                  <div key={itemIndex}>
+                  <div key={itemIndex} className='flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60'>
                     {/* title */}
-                    <div>{item.title}</div>
+                    <div className="font-light mb-2 md:mb-0">{item.title}</div>
                     <div className="hidden md:flex">-</div>
                     <div>{item.stage}</div>
                     <div className="flex gap-x-4">
                       {/* icons */}
                       {item.icons?.map((icon, itemIndex) => {
                         return (
-                          <div className="text-2xl transition-all duration-500">{icon}</div>
+                          <div className="text-2xl text-white">{icon}</div>
                         )
                       })}
                     </div>
