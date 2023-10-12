cacae14bcf5af797e63e61c61b15dbdf72fa4605
eduardo.cruz <tecnologiaesolucoespr@gmail.com>
2023-10-12 18:43:28 -0300

    geat: youtube

--- components/Nav.js
+++ components/Nav.js
@@ -12,7 +12,7 @@ import {
 export const navData = [
   { name: 'home', path: '/', icon: <HiHome /> },
   { name: 'about', path: '/about', icon: <HiUser /> },
-  { name: 'services', path: '/services', icon: <HiPlay /> },
+  { name: 'youtube', path: '/youtube', icon: <HiPlay /> },
   { name: 'work', path: '/work', icon: <HiViewColumns /> },
   {
     name: 'testimonials',
