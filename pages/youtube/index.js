1a4f0f2b90331e4e0f4dfe8214c89559e106e968
eduardo.cruz <tecnologiaesolucoespr@gmail.com>
2023-10-14 15:07:31 -0300

    feat: playvideo

--- pages/youtube/index.js
+++ pages/youtube/index.js
@@ -1,38 +1,106 @@
-import React, { useState } from 'react'
-import { motion } from 'framer-motion';
+import React, { useState, useRef , useEffect } from 'react'
+import { motion, useForceUpdate } from 'framer-motion';
 import { fadeIn } from '../../variants';
 
-const Services = () => {
-  const [link, setLink] = useState('');  
+import { HiPlay, HiStop } from 'react-icons/hi'
 
-  const handleChange = (event) => {
-    if(event){
-      var weblink = "https://www.youtube.com/embed/" + event.target.value.substr("https://www.youtube.com/watch?v=".length)+"?autoplay=1";
-      setLink(weblink);
-    }
-  };
+function usePlayState($videoPlayer){  
+  const [ playerState, setPlayerState ] = useState({
+    playing: false,
+    percentage: 0,
+    time: 0,
+  }); 
   
+  useEffect(() => {
+    playerState.playing ? $videoPlayer.current.play() : $videoPlayer.current.pause();
+  }, [
+    playerState.playing
+  ]);
+
+  function toggleVideoPlay() {    
+    setPlayerState({
+      ...playerState,
+      playing: !playerState.playing
+    })
+  }
+
+  function handleTimeUpdate() {
+    const currentPercentage = ($videoPlayer.current.currentTime / $videoPlayer.current.duration) * 100;
+    let current = parseInt(currentPercentage / 10) * 10;
+    setPlayerState({
+      ...playerState,
+      percentage: currentPercentage,
+      time: current + '%',
+      duration: $videoPlayer.current.duration
+    })  
+  }
+
+  function handleChangeVideoPercentage(event) {
+    const currentPercentageValue = event.target.value;
+    $videoPlayer.current.currentTime =  $videoPlayer.current.duration / 100 * currentPercentageValue;
+
+    setPlayerState({
+      ...playerState,
+      percentage: currentPercentageValue,
+    })
+  }
+
+  return {
+    playerState,
+    toggleVideoPlay,
+    handleTimeUpdate,
+    handleChangeVideoPercentage
+  }
+}
+
+const Services = () => { 
+  const $videoPlayer = useRef(null); 
+  const {playerState, toggleVideoPlay, handleTimeUpdate, handleChangeVideoPercentage }  = usePlayState($videoPlayer); 
+ 
   return (
-    <div className="flex h-full bg-primery/30 py-40 justify-center">
-      <div className="w-[100%] xl:w-[60%]  items-center justify-center">
-        {link && (
-          <iframe className="w-[100%] h-[48%] xl:w-[100%] xl:h-[100%] items-center justify-center" 
-            src={link}
-            title="Player Video" 
-            frameborder="0" 
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-            allowfullscreen="true"
-            autoplay="true"/>
-        )} 
-        <motion.input className="w-full m-2 p-2 mx-1 rounded-[3px] text-black" 
-            variants={fadeIn('top', 1.5)} 
-            initial='hidden' 
-            animate='show' 
-            exit='hidden'
-            type="text" onChange={handleChange}  placeholder="link youtube." />  
-         
+    <section className='h-full bg-primary/30'>
+      <div className='container mx-auto py-32 text-center xl:text-left flex  justify-center h-full w-full '>
+        <div className='flex w-full rounded-lg overflow-hidden mx-auto'>
+          <div class="flex flex-col">
+            <div class="relative items-center justify-center">
+              <video src='../../video.mp4' 
+                ref={$videoPlayer}
+                onTimeUpdate={handleTimeUpdate}
+                className='rounded xl:w-[1080px]'
+                poster=''/>
+            </div>
+
+            <div>
+              <div>
+                <input onChange={handleChangeVideoPercentage}
+                  value={playerState.percentage } 
+                  type='range' min='0' max='100' 
+                  className='relative h-1 bg-gray-200 w-full bg-green-500 '/>
+              </div>
+              <div class="flex justify-between text-xs font-medium text-gray-500 items-center p-2">
+                <div className='min-w-[25px]'>{ playerState.time }</div>
+
+                <div class="flex items-center justify-center">
+                  <button onClick={ toggleVideoPlay } className='focus:outline-none text-white-900'>
+                      <div className='text-[25px]'>{ playerState.playing ?  <HiStop /> : <HiPlay />}</div>                        
+                  </button>
+                </div>
+
+                <div className='min-w-[25px]'>100%</div>
+              </div>
+              
+            </div>
+            <motion.h2 
+                variants={fadeIn('down', 0.3)} 
+                initial="hidden" 
+                animate="show"  
+                exit="hidden"className='text-[18px] w-full text-center my-8 bt-2 rounded border border-white/50 py-8'>
+                  Video player desenvolvido em <span className='text-accent font-bold'>react</span>, com controles personalizados. 
+              </motion.h2>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
