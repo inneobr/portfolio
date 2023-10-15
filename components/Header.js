3f0ecab00d3dcc2bd06d7c5424a92b959d8a94cf
eduardo.cruz <tecnologiaesolucoespr@gmail.com>
2023-10-15 12:22:23 -0300

    feat: producao

--- components/Header.js
+++ components/Header.js
@@ -8,8 +8,8 @@ const Header = () => {
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
           {/* logo */}
-          <Link href={'/'}>
-            <Image className="w-[39px] h-[110px] xl:w-[220px] xl:h-[78px]"
+          <Link href={'/'} className="hidden xl:flex md:flex">
+            <Image className="xl:w-[220px] xl:h-[78px]"
               src={'/logo.svg'} 
               width={0} 
               height={0} 
