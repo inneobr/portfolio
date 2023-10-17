08263c043f38a322d31130412c08a6e9d6609d60
eduardo.cruz <tecnologiaesolucoespr@gmail.com>
2023-10-17 13:35:28 -0300

    feat: circles

--- components/Circles.js
+++ components/Circles.js
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 const Circles = () => {
   return (
-    <div className="w-[360px] xl:w-[360px] absolute -right-[190px] -bottom-[110px]
-      mix-blend-color-dodge animate-pulse duration-75 z-10 rotate-45" >
-        <Image src={'/circles2.png'} width={260} height={200} alt='' className="w-full h-full"/>
+    <div className="w-[360px] xl:w-[360px] absolute -right-[200px] -bottom-[150px]
+      mix-blend-color-dodge animate-pulse duration-75 z-10 rotate-90" >
+        <Image src={'/circle.png'} width={260} height={200} alt='' className="w-full h-full"/>
     </div>
   );
 };
